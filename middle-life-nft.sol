cb713e721d5032970a0f59079f63dec5c13c10ba
peng wang <pengwang@pengdeMacBook-Air.local>
2022-07-10 23:10:44 +0800

    clear ERC721 && update ERC721A usage

--- middle-life-nft.sol
+++ middle-life-nft.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./ERC721A.sol";
