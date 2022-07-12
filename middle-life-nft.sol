6df0d5e1ed69b94f6f8d81c5675bf30026086f41
peng wang <pengwang@pengdeMacBook-Air.local>
2022-07-12 11:22:58 +0800

    代码优化

--- middle-life-nft.sol
+++ middle-life-nft.sol
@@ -10,8 +10,8 @@ import "./ERC721A.sol";
 contract MidLife is ERC721A, Ownable {
     using Strings for uint256;
 
-    bool public _isSaleActive = false;
-    bool public _revealed = false;
+    bool public saleActived = false;
+    bool public revealed = false;
 
     // Constants
     uint256 public constant MAX_SUPPLY = 500;
@@ -23,9 +23,15 @@ contract MidLife is ERC721A, Ownable {
     string public baseExtension = ".json";
     bytes32 public merkleRoot = 0xabf0ea6f02025cc1b8d7556e47ebadb30ce1cb70560e650c8d09c0133700b983;
 
-    mapping(uint256 => string) private _tokenURIs;
+    // mapping(uint256 => string) private _tokenURIs;
 
     event Minted(address minter, uint256 amount);
+    event SetFlipSaleActive(bool isActive);
+    event SetRevealed(bool isRevealed);
+    event SetMintPriced(uint256 price);
+    event SetMaxMint(uint256 maxMint);
+    event SetNotRevealedURIed(string noRevealedUri);
+    event SetBaseExtensioned(string extension);
 
     constructor(string memory initBaseURI, string memory initNotRevealedUri)
         ERC721A("Middle Life", "ML")
@@ -34,24 +40,22 @@ contract MidLife is ERC721A, Ownable {
         setNotRevealedURI(initNotRevealedUri);
     }
 
-    function mintML(uint256 tokenQuantity, bytes32[] calldata _merkleProof) public payable {
-        require(_isSaleActive, "Sale must be active to mint Middle Life");
+    function mintML(uint256 tokenQuantity, bytes32[] calldata _merkleProof) external payable {
+        require(saleActived, "Sale must be active to mint Middle Life");
         require(totalSupply() + tokenQuantity <= MAX_SUPPLY, "Sale would exceed max supply");
 
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
         require(MerkleProof.verify(_merkleProof, merkleRoot, leaf), "Invalid Proof");
 
         require(_numberMinted(msg.sender) + tokenQuantity <= maxMint, "Can only mint 2 nft per wallet");
-
         require(tokenQuantity * mintPrice <= msg.value, "Not enough ether sent");
-        balances[msg.sender] = senderBalance + tokenQuantity;
 
         _safeMint(msg.sender, tokenQuantity);
         emit Minted(msg.sender, tokenQuantity);
     }
 
     function tokenURI(uint256 tokenId)
-        public
+        external
         view
         virtual
         override
@@ -62,21 +66,21 @@ contract MidLife is ERC721A, Ownable {
             "ERC721Metadata: URI query for nonexistent token"
         );
 
-        if (_revealed == false) {
+        if (revealed == false) {
             return notRevealedUri;
         }
 
-        string memory _tokenURI = _tokenURIs[tokenId];
+        // string memory _tokenURI = _tokenURIs[tokenId];
         string memory base = _baseURI();
 
         // If there is no base URI, return the token URI.
-        if (bytes(base).length == 0) {
-            return _tokenURI;
-        }
+        // if (bytes(base).length == 0) {
+        //     return _tokenURI;
+        // }
         // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
-        if (bytes(_tokenURI).length > 0) {
-            return string(abi.encodePacked(base, _tokenURI));
-        }
+        // if (bytes(_tokenURI).length > 0) {
+        //     return string(abi.encodePacked(base, _tokenURI));
+        // }
         // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
         return string(abi.encodePacked(base, tokenId.toString(), baseExtension));
     }
@@ -87,20 +91,24 @@ contract MidLife is ERC721A, Ownable {
     }
 
     //only owner
-    function flipSaleActive() public onlyOwner {
-        _isSaleActive = !_isSaleActive;
+    function flipSaleActive() external onlyOwner {
+        saleActived = !saleActived;
+        SetFlipSaleActive(saleActived);
     }
 
-    function flipReveal() public onlyOwner {
-        _revealed = !_revealed;
+    function flipReveal() external onlyOwner {
+        revealed = !revealed;
+        SetRevealed(revealed);
     }
 
-    function setMintPrice(uint256 _mintPrice) public onlyOwner {
+    function setMintPrice(uint256 _mintPrice) external onlyOwner {
         mintPrice = _mintPrice;
+        SetMintPriced(mintPrice);
     }
 
     function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
         notRevealedUri = _notRevealedURI;
+        SetNotRevealedURIed(notRevealedUri);
     }
 
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
@@ -108,14 +116,16 @@ contract MidLife is ERC721A, Ownable {
     }
 
     function setBaseExtension(string memory _newBaseExtension)
-        public
+        external
         onlyOwner
     {
         baseExtension = _newBaseExtension;
+        SetBaseExtensioned(baseExtension);
     }
 
-    function setMaxMint(uint256 _maxMint) public onlyOwner {
+    function setMaxMint(uint256 _maxMint) external onlyOwner {
         maxMint = _maxMint;
+        SetMaxMint(maxMint);
     }
 
     function withdraw(address payable to) external onlyOwner {
